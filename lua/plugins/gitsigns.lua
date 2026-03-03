1597ad2e258a76b24cecd0253606514b979e9882
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-03-03 05:35:18 -0800

    feat(gitsigns): add sensible minimal config
    most of the essential gitsigns functionality is
    exposed through Plug cmds. As such, this config is
    a good starting point for an in-editor git flow with
    operations like `diffthis` invokable through command
    line

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,35 @@
 return {
     'lewis6991/gitsigns.nvim',
-    opts = {
-        signs = {
-            add = { text = '+' },
-            change = { text = '~' },
-            delete = { text = '_' },
-            topdelete = { text = '‾' },
-            changedelete = { text = '~' },
-        },
-    },
+    -- opts = {
+    --     signs = {
+    --         add = { text = '+' },
+    --         change = { text = '~' },
+    --         delete = { text = '_' },
+    --         topdelete = { text = '‾' },
+    --         changedelete = { text = '~' },
+    --     },
+    -- },
+    config = function ()
+        local gitsigns = require('gitsigns')
+
+        gitsigns.setup({
+
+        })
+
+        vim.keymap.set('n', ']g', function()
+            ---@diagnostic disable-next-line: missing-fields
+            gitsigns.nav_hunk('next', { preview = true, greedy = true })
+        end, { desc = '' })
+
+        vim.keymap.set('n', '[g', function()
+            ---@diagnostic disable-next-line: missing-fields
+            gitsigns.nav_hunk('prev', { preview = true })
+        end, { desc = '' })
+
+        vim.keymap.set('n', 'gp', function()
+            gitsigns.preview_hunk()
+        end, { desc = '' })
+
+    end,
 }
 -- vim: ts=4 sts=4 sw=4 et
