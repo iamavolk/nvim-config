80598b055fe3789e6187531344e4e740b8219d71
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-03-03 07:24:47 -0800

    feat(nvim-cmp): change keybinds

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,7 +2,6 @@ return {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
         {
             'L3MON4D3/LuaSnip',
             build = (function()
@@ -28,17 +27,14 @@ return {
         },
         'saadparwaiz1/cmp_luasnip',
 
-        -- Adds other completion capabilities.
-        --  nvim-cmp does not ship with all sources by default. They are split
-        --  into multiple repos for maintenance purposes.
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-cmdline',
         'hrsh7th/cmp-nvim-lsp-signature-help',
     },
 
     config = function()
-        -- See `:help cmp`
         local cmp = require('cmp')
         local luasnip = require('luasnip')
         luasnip.config.setup({})
@@ -49,25 +45,36 @@ return {
                     luasnip.lsp_expand(args.body)
                 end,
             },
-            completion = { completeopt = 'menu,menuone,noinsert' },
+            completion = {
+                completeopt = 'menu,menuone,noinsert',
+                -- autocomplete = false,
+                keyword_length = 3,
+            },
+            window = {
+                completion = {
+                    border = "single",
+                    max_height = 15,
+                },
+                documentation = {
+                    border = "single",
+                },
+            },
 
-            -- `:help ins-completion`
             mapping = cmp.mapping.preset.insert {
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<A-n>'] = cmp.mapping.select_next_item({ count = 4 }),
+                ['<A-p>'] = cmp.mapping.select_prev_item({ count = 4 }),
 
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-p>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-n>'] = cmp.mapping.scroll_docs(4),
 
                 --  This will auto-import if your LSP supports it.
                 --  This will expand snippets if the LSP sent a snippet.
-                ['<C-m>'] = cmp.mapping.confirm { select = true },
+                ['<C-i>'] = cmp.mapping.confirm { select = true },
 
-                
                 -- Manually trigger a completion from nvim-cmp.
                 --  Generally you don't need this, because nvim-cmp will display
                 --  completions whenever it has completion options available.
-                ['<A-Space>'] = cmp.mapping.complete {},
+                ['<C-m>'] = cmp.mapping.complete {},
 
                 ['<C-f>'] = cmp.mapping(function()
                     if luasnip.expand_or_locally_jumpable() then
@@ -97,6 +104,28 @@ return {
                 { name = 'nvim_lsp_signature_help' },
             },
         }
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'buffer' }
+            }
+        })
+
+        cmp.setup.cmdline(':', {
+            completion = {
+                completeopt = 'menu,menuone,noinsert',
+                -- autocomplete = false,
+            },
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                { name = 'cmdline' }
+            }),
+            ---@diagnostic disable-next-line: missing-fields
+            matching = { disallow_symbol_nonprefix_matching = false }
+        })
+
     end,
 }
 -- vim: ts=4 sts=4 sw=4 et
