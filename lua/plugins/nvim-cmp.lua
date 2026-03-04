0a0601375493213383655255db437ac6d07fde2c
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-03-04 05:31:43 -0800

    fix(nvim-cmp): revome <C-i> due to interference in insert mode

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -69,7 +69,7 @@ return {
 
                 --  This will auto-import if your LSP supports it.
                 --  This will expand snippets if the LSP sent a snippet.
-                ['<C-i>'] = cmp.mapping.confirm { select = true },
+                -- ['<C-i>'] = cmp.mapping.confirm { select = true },
 
                 -- Manually trigger a completion from nvim-cmp.
                 --  Generally you don't need this, because nvim-cmp will display
