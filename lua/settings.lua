034eb360a7ed6e8a060d3c06a0999378a3359f8c
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-06-07 16:26:07 -0700

    feat(osc52 clipboard): set osc52 explicitly
    conditionally set osc52 (if ssh'd into)
    for the time being, this is the only approach
    that solves the asymmetry in nvim clipboard
    functionality when localhost works fine with
    both egress and ingress whereas remote host
    is non-functional with egress

--- lua/settings.lua
+++ lua/settings.lua
@@ -41,4 +41,19 @@ vim.opt.writebackup     = false
 vim.opt.swapfile        = false
 vim.opt.wrap            = false
 
+
+if vim.env.SSH_TTY then
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+        },
+    }
+end
+
 vim.cmd.packadd('nohlsearch')
