51674bf25336683eab49bec996570f48fb8ed9af
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-06-07 02:31:15 -0700

    feat(lsp): extract lua_ls from main lsp config

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -1,4 +1,31 @@
 return {
+    on_init = function(client, init_result)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath("config") and
+                (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+            then return end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+            runtime = {
+                version = "LuaJIT",
+                path = { "lua/?.lua", "lua/?/init.lua" },
+            },
+            workspace = {
+                checkThirdParty = false,
+                -- Pulls in all of runtime 
+                -- library = vim.api.nvim_get_runtime_file("", true),
+
+                library = {
+                        vim.env.VIMRUNTIME,
+                        '${3rd}/luv/library',
+                        '${3rd}/busted/library',
+                    }
+            },
+        })
+    end,
+
     on_attach = function(client, bufnr)
         client.server_capabilities.semanticTokensProvider = nil
     end,
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -16,45 +16,7 @@ return {
             } or {},
         }
 
-        vim.lsp.config('lua_ls', {
-
-            on_init = function(client, init_result)
-                if client.workspace_folders then
-                    local path = client.workspace_folders[1].name
-                    if path ~= vim.fn.stdpath("config") and
-                        (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
-                    then return end
-                end
-
-                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                    runtime = {
-                        version = "LuaJIT",
-                        path = { "lua/?.lua", "lua/?/init.lua" },
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        -- Pulls in all of runtime 
-                        -- library = vim.api.nvim_get_runtime_file("", true),
-
-                        -- library = {
-                        --     vim.env.VIMRUNTIME,
-                        --     '${3rd}/luv/library',
-                        --     '${3rd}/busted/library',
-                        -- }
-                    },
-                })
-            end,
-
-            --[[ TODO: extra capabilities might already be implemented in nvim core
-            --   Hence, this might not be needed anymore
-            --]]
-            -- capabilities = vim.tbl_deep_extend( 'force',
-            --     vim.lsp.protocol.make_client_capabilities(),
-            --     require('cmp_nvim_lsp').default_capabilities()),
-                -- require("blink.cmp").get_lsp_capabilities()),
-        })
-
         vim.lsp.enable('lua_ls')
-        -- vim.lsp.enable('ty')
+        vim.lsp.enable('ty')
     end,
 }
