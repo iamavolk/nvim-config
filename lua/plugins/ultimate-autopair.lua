3b27fce70a5e3c00eaf5db4d1ab7afd06ca75d30
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-06-07 02:19:50 -0700

    feat(ultimate-autopair): restore <c-i> in i as indent

--- lua/plugins/ultimate-autopair.lua
+++ lua/plugins/ultimate-autopair.lua
@@ -1,20 +1,20 @@
 return {
     'altermo/ultimate-autopair.nvim',
-    event={'InsertEnter','CmdlineEnter'},
-    branch='v0.6', --recommended as each new version will have breaking changes
+    event = {'InsertEnter','CmdlineEnter'},
+    branch = 'v0.6', --recommended as each new version will have breaking changes
 
-    opts={
+    opts = {
         {'$','$',fly=true,dosuround=true,newline=false,space=true, ft={'markdown', 'typst'}},
         {'$$','$$',fly=true,dosuround=true,newline=true,space=true, ft={'markdown'}},
         {'_','_',fly=false,dosuround=true,newline=false,space=true, ft={'markdown'}},
         {'**','**',fly=true,dosuround=true,newline=false,space=true, ft={'markdown'}},
         {'<','>',fly=true,dosuround=true,newline=true,space=true, ft={'markdown'}},
 
-        fastwarp = { map='<C-f>', rmap='<C-b>' },
-        -- bs = { map = { '<BS>', '<C-h>' } },
+        fastwarp = { map='<A-f>', rmap='<A-b>' },
+        bs = { map = { '<BS>', '<C-h>' } },
         cr = { map = '<C-m>' },
         close = { map='<A-a>' },
-        tabout = { enable = true, map = '<C-i>', hopout = true },
+        tabout = { enable = true, map = '<C-a>', hopout = true },
     }
 }
 -- vim: ts=4 sts=4 sw=4 et
