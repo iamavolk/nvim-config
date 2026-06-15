5a9e8a0f783d7efee724bea3fb33e04acbe014f9
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-06-15 11:17:07 -0700

    render-markdown: add lazy loading

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,4 +1,5 @@
 return {
+    lazy = true,
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
     opts = {},
