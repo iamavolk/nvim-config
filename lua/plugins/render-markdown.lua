19d4c0e43391545186d273fb5afcb2664bde18e2
Alex Volkov <alexfromshermanoaks@eecs.berkeley.edu>
2026-07-12 10:26:02 -0700

    feat(render-markdown): improve default rendering
    hover markdown files are primarily read-only, so
    no need to ad-hoc md source code under the cursor.
    also, center aligning code blocks tends to grab reader's
    focus a bit better

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,22 @@
 return {
-    lazy = true,
+    -- lazy = true,
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-    opts = {},
+    -- opts = {
+    --     heading = {
+    --         enabled = true,
+    --     },
+    -- },
+    config = function()
+        require('render-markdown').setup({
+            code = {
+                language = false,
+                width = 'block',
+                left_margin = 0.5,
+                left_pad = 0.2,
+                right_pad = 0.2,
+            },
+            anti_conceal = { enabled = false },
+        })
+    end,
 }
